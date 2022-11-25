f69abcbb5e0ee509e36bb97aa3ee0fcc021aa44a
Cengiz YILMAZ <clgnhmsi@gmail.com>
2022-11-25 22:18:31 +0300

    .

--- src/components/LoadingRock.vue
+++ src/components/LoadingRock.vue
@@ -1,22 +1,22 @@
 <template>
-    <a-result v-if="isShow" title="Öyle vurma ölmem">
-    <template #extra>
-        <img src="../assets/throw-rock.webp" alt="" />
-    </template>
-  </a-result>
+  <a-result v-if="isShow" title="Öyle vurma ölmem">
+  <template #extra>
+      <img src="../assets/throw-rock.webp" alt="" />
+  </template>
+</a-result>
 </template>
 <script>
 export default {
-  name: 'LoadingRock',
-  props: {
-    isShow: Boolean
-  }
+name: 'LoadingRock',
+props: {
+  isShow: Boolean
+}
 }
 
 </script>
 <style scoped>
 img {
-  margin: auto;
-  height: 50vh;
+margin: auto;
+height: 50vh;
 }
 </style>
